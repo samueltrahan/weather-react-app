2cb16eaa9b9d0a529d64539490ee4887bdbd7939
Samuel Trahan <samtrahan91@gmail.com>
2020-07-04 22:01:58 -0500

    api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Weather from './Weather';
+
 
 function App() {
+  const [weather, setWeather] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('Lafayette, Louisiana')
+  const API_KEY = process.env.API_KEY;
+
+  useEffect( () => {
+    getWeather();
+  }, [])
+
+  const getWeather = async () => {
+    const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`)
+    const data = await response.json();
+    setWeather(data)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+     <h1>Weathered</h1>
+    <form>
+      <input type="text"/>
+      <button>Search</button>
+    </form>
+    <div>
+      <Weather />
     </div>
+   </div>
   );
 }
 
---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Weather() {
+    return (
+        <div>
+            <h1>Today's weather</h1>
+            <h3>Week's Weather</h3>
+        </div>
+    )
+}
