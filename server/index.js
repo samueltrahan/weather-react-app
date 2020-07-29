6fd65ffde146e045c26386a37ff784acbe7728d5
Samuel Trahan <samtrahan91@gmail.com>
2020-07-28 21:12:46 -0500

    server set up

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const twilio = require('twilio');
+const express = require('express');
+const cors = require('cors');
+const Twilio = require('twilio');
+require('dotenv').config()
 
-const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID;
-const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN
-const client = (accountSid, authToken)
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioClient = new Twilio(
+   accountSid,
+   authToken
+  );
 
 const app = express(); //alias
 
@@ -12,17 +16,19 @@ app.use(cors()); //blocks the browser from restricting any data
 
 //welcome page for the server
 app.get('/', (req, res) => {
-    res.send('Welcome to the Express Server');
-})
+  res.send('Welcome to the Express Server');
+});
 
 app.get('/send-text', (req, res) => {
-    const { recipient,textmessage } = req.query
+  const { recipient, textmessage } = req.query;
 
-    client.messages.create({
-            body: textmessage,
-            to: recipient,
-            from: process.env.REACT_APP_TWILIO_PHONE_NUMBER,
-          }).then((message) => console.log(message.body))
-    });
+  twilioClient.messages
+    .create({
+      body: textmessage,
+      to: '+1' + recipient,
+      from: process.env.TWILIO_PHONE_NUMBER,
+    })
+    .then(message => console.log(message.body));
+});
 
-app.listen(4000, () => console.log('Running on port 4000'))
+app.listen(4000, () => console.log('Running on port 4000'));
