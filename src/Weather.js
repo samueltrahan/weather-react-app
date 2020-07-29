62d6aa7a48deebc5c361d07d9635107f4f66a9d8
Samuel Trahan <samtrahan91@gmail.com>
2020-07-28 19:13:21 -0500

    weather card added

--- src/Weather.js
+++ src/Weather.js
@@ -2,14 +2,23 @@ import React from 'react'
 
 export default function Weather({name, weather, description, temperature}) {
     return (
-        <div>
-            <h1>Today's Weather</h1>
-            {name}<br/><br/>
-            {weather}
-            <h3>Details </h3>
-            {description}
-            <h3>Temperature</h3>
-            {temperature} &deg;F
+    <>
+        <div class="col s12 m7">
+        <h2 class="header">{name}'s Weather</h2>
+        <div class="card horizontal">
+        <div class="card-image">
         </div>
+        <div class="card-stacked">
+        <div class="card-content">
+        <h3>{weather}</h3>
+        <h3>{description}</h3>
+        <h4>Temp: <br/><br/>{temperature} &deg;F</h4>
+        </div>
+        <div class="card-action">
+        </div>
+      </div>
+    </div>
+  </div>
+    </>
     )
 }
